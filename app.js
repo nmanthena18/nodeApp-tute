956bf74a99fd293f954ac8ea93c85952c42f17dd
nmanthena18 <nmanthena@ctepl.com>
2018-03-01 12:29:29 +0530

    setup

--- app.js
+++ app.js
@@ -86,4 +86,10 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+const port = process.env.PORT || '3000'
+
+app.listen(port, ()=> {
+	console.log(`app listening at ${port}`)
+})
+
 module.exports = app;
--- db/mongoose.js
+++ db/mongoose.js
@@ -1,5 +1,4 @@
 const  mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-console.log(process.env.PORT)
-mongoose.connect(prosess.env.MONGODB_URI || 'mongodb://localhost:27017/myDB-1');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/myDB-1');
 module.exports = {mongoose}
