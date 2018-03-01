9306ab8e1aa41f9b29347d541d5e5ff3052fd661
nmanthena18 <nmanthena@ctepl.com>
2018-03-01 12:19:02 +0530

    setup

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,4 +1,5 @@
 const  mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/myDB-1');
+console.log(process.env.PORT)
+mongoose.connect(prosess.env.MONGODB_URI || 'mongodb://localhost:27017/myDB-1');
 module.exports = {mongoose}
